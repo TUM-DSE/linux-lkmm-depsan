486f643760cd783462b2aadebf1c9668215cb59a
ReimersS <sebastian.reimers@tum.de>
2026-04-14 07:13:54 +0000

    Avoid blockaddr on arm through asm

--- arch/arm64/include/asm/linkage.h
+++ arch/arm64/include/asm/linkage.h
@@ -43,8 +43,7 @@
 	SYM_TYPED_START(name, SYM_L_GLOBAL, SYM_A_ALIGN)	\
 	bti c ;
 
-#define _THIS_IP_ ({ unsigned long __ip; asm volatile("adr %0, ." : "=r" (__ip)); __ip; })
-
 #define __bss_pgtbl __section(".bss..pgtbl") __aligned(PAGE_SIZE)
+#define _THIS_IP_ ({ unsigned long __here; asm ("adr %0, 1f\n1:" : "=r" (__here)); __here; })
 
 #endif
